e233f9cbda3ead6033688463d006fe4d377223df
harshitha9988 <harshitha.uppala@outlook.com>
2025-01-30 21:26:33 -0500

    operator precedence program

---
+++ operator precedence.py
@@ -0,0 +1,18 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v+w)*x/y;
+print("Value of (v=w)*x/y is",z)
+
+
+
+name= "Harshi"
+age=11
+
+if name == "Harshi" or name == "John" and age >=2:
+    print("Hello! Welcome")
+
+else:
+    print("Good byeee!")
